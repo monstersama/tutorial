e4bdde90a51941f520ff4b676462c192262431f6
monstermoon <yuntian.hee@gmail.com>
2019-05-02 15:22:50 +0800

    tutorial

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -1,21 +1,39 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from tutorial.items import QuoteItem
+
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
-
-    def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+    # def start_requests(self):
+    #     urls = [
+    #         'http://quotes.toscrape.com/page/1/',
+    #         'http://quotes.toscrape.com/page/2/',
+    #     ]
+    #     for url in urls:
+    #         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html'%page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s'%filename)
+        # page = response.url.split("/")[-2]
+        # filename = 'quotes-%s.html'%page
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
+        # self.log('Saved file %s'%filename)
+        quotes = response.css('div.quote')
+        for quote in quotes:
+            item = QuoteItem()
+            text = quote.css('.text::text').extract_first()
+            author = quote.css('.author::text').extract_first()
+            tags = quote.css('.tags .tag::text').extract()
+            item['text'] = text
+            item['author'] = author
+            item['tags'] = tags
+            yield item
+
+
+        next = response.css('.pager .next a::attr(href)').extract_first()
+        url = response.urljoin(next)
+        yield scrapy.Request(url=url, callback=self.parse)
